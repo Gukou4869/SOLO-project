207f764fde043ae677275f34c3d82fc153c27c06
谷口翔太 <taniguchishouta@taniguchishoutanoMacBook-Pro.local>
2021-02-08 14:34:31 +0900

    Finished setup of knexfile

---
+++ server/knexfile.js
@@ -0,0 +1,12 @@
+const config = {
+  client: "pg",
+  connection: {
+    connectionString:
+      process.env.DATABASE_URL ||
+      `postgres://${process.env.USER}:${process.env.PW}@127.0.0.1:5432/truckstop`,
+    ssl: process.env.HAS_SSL ? { rejectUnauthorized: false } : undefined,
+  },
+  searchPath: "public",
+};
+
+module.exports = config;
